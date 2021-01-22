4c5ec72bde06a5c31453037500f312fb6dfa6811
godo <godo@AL01590068.local>
2021-01-22 10:53:51 +0900

    title 수정

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "NHN 베이스캠프 1주차 소감",
         "excerpt":"NHN 베이스 캠프 1주차가 끝났다.  많은 양의 과제를 내주었는데, 구체적인 양으로는 책 약 300페이지를 구현하고 시험을 보았다.   현재 이 글 역시, 베이스 캠프의 과제이기도 하다.   다음주부터 양이 엄청 방대해진다고 하는데..  개발자로 단기간에 빠르게 성장할 수 있을것 같아 기대가 된다.  ","categories": ["Blog"],
         "tags": ["Blog"],
-        "url": "/blog/first-post/",
+        "url": "https://changguhan.github.io/blog/first-post/",
         "teaser": null
       }]
